a256c7c869327c2226ede2b49e0c7a64df29b9ee
Parmesh Ghanvat <Parmesh.Ghanvat@sigma-systems.com>
2016-05-04 15:13:01 +0530

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,9 @@
+var name = getQueryVariable('name');
+var room = getQueryVariable('room');
+
 var socket = io();
+
+console.log(name + ' wants to join ' + room);
 
 socket.on('connect', function () {
 	console.log('Connected to Socket.io Server');
@@ -6,10 +11,13 @@ socket.on('connect', function () {
 
 socket.on('message', function (message) {
 	var momentTimeStamp = moment.utc(message.timestamp);
+	var $message = jQuery('.messages');
+
 	console.log('New message:');
 	console.log(message.text);
-
-	jQuery('.messages').append('<p><strong>'+momentTimeStamp.local().format('h:mm a')+'</strong>   ' +message.text+'</p>');
+    
+	$message.append('<p><strong> '+message.name+' </strong>' +'<strong>'+momentTimeStamp.local().format('h:mm a')+'</strong></p>');
+	$message.append('<p>'+message.text+'</p>');
 });
 
 // Handles submitting new message
@@ -19,6 +27,7 @@ $form.on('submit',function (event) {
 	event.preventDefault();
 	var $message = $form.find('input[name=message]');
 	socket.emit('message',{
+		name:name,
 		text:$message.val()
 	});
 
