2b048e6fc1178f6b4dbdf54c5e54e179c7478cac
Parmesh Ghanvat <Parmesh.Ghanvat@sigma-systems.com>
2016-05-04 09:59:23 +0530

    Added custome message event

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,10 @@
 var socket = io();
 
-socket.on('connect', function () {
+socket.on('connect', function (socket) {
 	console.log('Connected to Socket.io Server');
+});
+
+socket.on('message', function (message) {
+	console.log('New message:');
+	console.log(message.text);
 });
